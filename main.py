54e740b649630f9b9088fcc30004faf5c55b7349
Brayan Josue Jacome Noroña <brayanjacome02@gmail.com>
2025-07-22 10:12:55 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from routes.dog_routes import router as dog_router
+import os
+import uvicorn
 
 app = FastAPI()
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -20,3 +23,7 @@ async def startup_db_client():
 @app.get("/")
 async def root():
     return {"message": "API is running"}
+
+if __name__ == "__main__":
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
